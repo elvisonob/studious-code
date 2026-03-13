efea22ec4b76e53c26d0f30ab08bb4ed2d5f27ab
elvisonob <elvisonob@yahoo.com>
2026-03-13 05:35:25 +0000

    applying tailwind

--- src/component/App.js
+++ src/component/App.js
@@ -1,3 +1,17 @@
 export default function App() {
-  return <div>One Love</div>;
+  return (
+    <div>
+      <form className="bg-blue-500">
+        <label>Text</label>
+        <input
+          type="text"
+          className="block ml-10 mr-10 w-full focus:w-96"
+          placeholder="Enter something"
+        />
+        <button className=" border p-5 m-5 border-red-500 font-bold bg-orange-500 rounded-full">
+          Click
+        </button>
+      </form>
+    </div>
+  );
 }
