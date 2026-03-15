2b45c5f97e9770b75cc2609489970a0d32f522e5
elvisonob <elvisonob@yahoo.com>
2026-03-15 10:07:28 +0000

    tailwind-navigation

--- src/component/App.js
+++ src/component/App.js
@@ -1,17 +1,37 @@
+import image from '../assets/ono.jpg';
+
 export default function App() {
   return (
     <div>
-      <form className="bg-blue-500">
-        <label>Text</label>
-        <input
-          type="text"
-          className="block ml-10 mr-10 w-full focus:w-96"
-          placeholder="Enter something"
-        />
-        <button className=" border p-5 m-5 border-red-500 font-bold bg-orange-500 rounded-full">
-          Click
-        </button>
-      </form>
+      <nav className="mx-auto p-4 bg-amber-400">
+        <div className="container mx-auto flex items-center justify-between">
+          <a
+            href="#"
+            className="focus:outline-none focus-visible:ring-4 ring-white ring-offset-4 ring-offset-amber-400 rounded-sm lg:absolute lg:left-1/2 lg:-translate-x-1/2 z-50 hover:opacity-75 transition-opacity"
+          >
+            <img src={image} className="w-48 md:w-64 lg:w-32" alt="logo" />
+          </a>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-8 h-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+              />
+            </svg>
+          </button>
+        </div>
+      </nav>
+      <header></header>
+      <main></main>
+      <section></section>
     </div>
   );
 }
